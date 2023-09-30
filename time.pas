55327bffa9c9a17f459349cae79eefcfef3ac4e0
Mlynnik <mlynnik@sfedu.ru>
2023-09-30 11:16:05 +0300

    output days in year

--- time.pas
+++ time.pas
@@ -1,7 +1,7 @@
 ﻿begin
   var Year := ReadInteger('Введите год');
-  var flag := False;
+  var days := 365;
   if (Year mod 4 = 0) and ((Year mod 100 <> 0) or (Year mod 400 = 0)) then
-    flag := True;
-  Print($'Високосность: {flag}.');
+    days += 1;
+  Print($'Количество: {days}.');
 end.
